f1a50c9ce07f83f0475f2c8ef7d48f6577351e2d
tkaii <57178134+tkaii@users.noreply.github.com>
2020-03-17 17:08:38 +0900

    Add files via upload

---
+++ mysite_2-masterのコピー/index.php
@@ -0,0 +1,51 @@
+<?php get_header(); ?>
+
+
+<main>
+    <section class="blog-intro">
+      <p>初めまして。 ブログを見に来てくださり、ありがとうございます。Ai と申します。</p>
+      <h2>Welcome to</h2>
+      <h1 class="blog-title">My Blog !</h1>
+      <p>
+        ウェブデザイン・ワードプレスのサイト作成、記事の執筆など、サイト制作のお手伝いをしています。動物、自然、毎日の街・空・景色の中のきれいを見つけるのが大好きな人間です。
+      </p>
+    </section>
+
+<?php get_template_part('loop'); ?>
+
+    <section class="img-wrapper">
+      <h2>
+        Make your life more fun!
+      </h2>
+      <div class="img-lg">
+        <svg id="コンポーネント_5_1" data-name="コンポーネント 5 – 1" xmlns="http://www.w3.org/2000/svg" width="296.164" height="343.904" viewBox="0 0 296.164 343.904">
+          <defs>
+          </defs>
+          <g id="グループ_8" data-name="グループ 8" transform="translate(-112.387 -456.602)">
+            <line id="線_4" data-name="線 4" class="cls-1" x2="1" y2="237" transform="translate(374.5 483.5)"/>
+            <path id="パス_5" data-name="パス 5" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(0.978, -0.208, 0.208, 0.978, 342.387, 463.325)"/>
+            <path id="パス_6" data-name="パス 6" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(-0.259, 0.966, -0.966, -0.259, 408.551, 463.59)"/>
+          </g>
+          <g id="グループ_9" data-name="グループ 9" transform="translate(-262.387 -376.602)">
+            <line id="線_4-2" data-name="線 4" class="cls-1" x2="1" y2="237" transform="translate(374.5 483.5)"/>
+            <path id="パス_5-2" data-name="パス 5" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(0.978, -0.208, 0.208, 0.978, 342.387, 463.325)"/>
+            <path id="パス_6-2" data-name="パス 6" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(-0.259, 0.966, -0.966, -0.259, 408.551, 463.59)"/>
+          </g>
+          <g id="グループ_10" data-name="グループ 10" transform="translate(-182.387 -406.602)">
+            <line id="線_4-3" data-name="線 4" class="cls-1" x2="1" y2="237" transform="translate(374.5 483.5)"/>
+            <path id="パス_5-3" data-name="パス 5" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(0.978, -0.208, 0.208, 0.978, 342.387, 463.325)"/>
+            <path id="パス_6-3" data-name="パス 6" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(-0.259, 0.966, -0.966, -0.259, 408.551, 463.59)"/>
+          </g>
+          <g id="グループ_11" data-name="グループ 11" transform="translate(-342.387 -406.602)">
+            <line id="線_4-4" data-name="線 4" class="cls-1" x2="1" y2="237" transform="translate(374.5 483.5)"/>
+            <path id="パス_5-4" data-name="パス 5" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(0.978, -0.208, 0.208, 0.978, 342.387, 463.325)"/>
+            <path id="パス_6-4" data-name="パス 6" class="cls-2" d="M14,0A13.757,13.757,0,0,1,28,13.5V27H14A13.757,13.757,0,0,1,0,13.5,13.757,13.757,0,0,1,14,0Z" transform="matrix(-0.259, 0.966, -0.966, -0.259, 408.551, 463.59)"/>
+          </g>
+        </svg>
+      </div>
+    </section>
+  </main>
+
+
+
+<?php get_footer(); ?>
